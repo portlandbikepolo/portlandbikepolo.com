ce88ff41aeaa34a2324cd5fff730df50a826612c
Brandon Catubig <bcatubig@pm.me>
2026-02-06 23:22:54 -0800

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+	output: "static",
 
   experimental: {
     fonts: [
