c3c1a0354f83f876c687c0bc8bea5e9f0e067147
Brandon Catubig <bcatubig@pm.me>
2026-03-20 16:41:01 -0700

    reset

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 
@@ -6,28 +5,13 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
+  output: "static",
 
   experimental: {
-    fonts: [
-      {
-        name: "Inter",
-        cssVariable: "--ff-inter",
-        provider: fontProviders.fontsource(),
-        weights: [400, 700],
-      },
-    ],
+    fonts: [],
   },
 
   integrations: [react(), markdoc(), keystatic()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  adapter: cloudflare(),
 });
